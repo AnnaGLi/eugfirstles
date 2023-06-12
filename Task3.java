5069a111b54f9d96624e12b8bad8d325befc2f5e
AnnaGLi <106550070+AnnaGLi@users.noreply.github.com>
2023-06-13 00:07:52 +0200

    Update Task3.java
    new commit

--- Task3.java
+++ Task3.java
@@ -3,14 +3,11 @@ package org.example;
 
 import java.util.Random;
 
-import static java.lang.Integer.MAX_VALUE;
 import static java.lang.Short.MAX_VALUE;
-
 
 public class Task3 {
     public static void main(String[] args) {
-        //создаем массив m1
-        //int[] m1 = new int[36];
+       
         int num = new Random().nextInt(0, 255); //число, для которого надо найти кратные
         System.out.println(num);
 
