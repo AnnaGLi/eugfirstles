aa8f1b0706fab3132df554250bba6d62948cf870
AnnaGLi <106550070+AnnaGLi@users.noreply.github.com>
2023-06-12 20:43:06 +0200

    Add files via upload
    task1

---
+++ Task2.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.util.Random;
+
+public class Task2 {
+        public static void task2 (String[]args){
+            int i;
+            Random num=new Random();
+            i=num.nextInt(0,2001);
+            System.out.println(i);
+        }
+    }
